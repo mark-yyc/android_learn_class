7bbf01518f35855e812c45897317aec59e85ac97
myname <mymail>
2021-04-12 11:41:49 +0800

    chapter7 finished

---
+++ Chapter7/app/src/main/java/com/domker/study/androidstudy/MediaPlayerActivity.java
@@ -0,0 +1,138 @@
+package com.domker.study.androidstudy;
+
+import android.graphics.PixelFormat;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.SeekBar;
+
+import java.io.IOException;
+
+public class MediaPlayerActivity extends AppCompatActivity {
+    private SurfaceView surfaceView;
+    private MediaPlayer player;
+    private SurfaceHolder holder;
+    private SeekBar seekBar;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("MediaPlayer");
+
+        setContentView(R.layout.layout_media_player);
+        surfaceView = findViewById(R.id.surfaceView);
+
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(getResources().openRawResourceFd(R.raw.big_buck_bunny));
+            holder = surfaceView.getHolder();
+            holder.setFormat(PixelFormat.TRANSPARENT);
+            holder.addCallback(new PlayerCallBack());
+            player.prepare();
+            player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    // 自动播放
+                    player.start();
+                    player.setLooping(true);
+                    new SeekBarThread().start();
+                }
+            });
+
+            player.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+                @Override
+                public void onBufferingUpdate(MediaPlayer mp, int percent) {
+                    System.out.println(percent);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        findViewById(R.id.buttonPlay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                player.start();
+            }
+        });
+
+        findViewById(R.id.buttonPause).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                player.pause();
+            }
+        });
+
+        seekBar=findViewById(R.id.mediaSeekBar);
+        seekBar.setMax(player.getDuration());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    player.seekTo(seekBar.getProgress());
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        findViewById(R.id.buttonReset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                player.seekTo(0);
+                seekBar.setProgress(0);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (player != null) {
+            player.stop();
+            player.release();
+        }
+    }
+
+    private class PlayerCallBack implements SurfaceHolder.Callback {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            player.setDisplay(holder);
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+
+        }
+    }
+
+    private class SeekBarThread extends Thread{
+        @Override
+        public void run(){
+            super.run();
+            while(seekBar.getProgress()<=seekBar.getMax()){
+                seekBar.setProgress(player.getCurrentPosition());
+            }
+        }
+    }
+}
